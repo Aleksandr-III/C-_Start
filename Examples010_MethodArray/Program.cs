4107fec9f8c0213574db23bc1e27ec178c65bb9d
Александр <popovaleksandr201306@gmail.com>
2022-08-15 02:17:12 +0500

    метод + массив

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать (в данном случае как только будет совпадение)
+    }
+    // index = index + 1
+    index++;
+}
